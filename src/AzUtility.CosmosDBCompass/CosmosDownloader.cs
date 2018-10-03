a93e163842f4b3406f96ef7c3f2996472d9d6e28
Wuyi Weng <wuweng@microsoft.com>
2018-10-03 14:31:45 -0700

    - add account parser

---
+++ src/AzUtility.CosmosDBCompass/CosmosDownloader.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzUtility.CosmosDBCompass
+{
+    public class CosmosDownloader
+    {
+        private readonly DocumentClient _client;
+        private readonly string _baseFolder;
+
+        public CosmosDownloader(DocumentClient client,string baseFolder)
+        {
+            _client = client;
+            _baseFolder = baseFolder;
+        }
+
+        public async Task RunAsync()
+        {
+            Console.WriteLine($"Begin downloading data to folder: {_baseFolder}");
+
+            var databases = await _client.ReadDatabaseFeedAsync();
+            foreach (var db in databases)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
---
+++ src/AzUtility.CosmosDBCompass/Program.cs
@@ -0,0 +1,43 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace AzUtility.CosmosDBCompass
+{
+    class Program
+    {
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option("-b", "Backup.", CommandOptionType.NoValue)]
+        public bool Backup { get; }
+
+        [Option("-d", "Relative or absolute path to the folder that you want to upload or download to.", CommandOptionType.SingleValue)]
+        [Required]
+        public string Dir { get; }
+
+        [Option("-c", "CosmosDB connection string.", CommandOptionType.SingleValue)]
+        [Required]
+        public string ConnectionString { get; set; }
+
+        public void OnExecute()
+        {
+            var mode = Backup ? "Download" : "Upload";
+            Console.WriteLine($"Mode: {mode}");
+
+            if (DocumentDbAccount.TryParse(ConnectionString, out var client))
+            {
+                var fullDir = Path.GetFullPath(Dir);
+                if (Backup)
+                {
+                    var downloader = new CosmosDownloader(client, fullDir);
+                    downloader.RunAsync().Wait();
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Connection String is invalid.");
+            }
+        }
+    }
+}
