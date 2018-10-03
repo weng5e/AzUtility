d19f711d7cce957d6b76c6452402277444284eab
Wuyi Weng <wuweng@microsoft.com>
2018-10-02 21:34:33 -0700

    - implement uploader.

---
+++ src/AzUtility.BlobUploader/FolderUploader.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzUtility.BlobUploader
+{
+    public class FolderUploader
+    {
+        private readonly CloudBlobClient _cloudBlobClient;
+        private readonly string _baseFolder;
+
+        public FolderUploader(CloudStorageAccount storageAccount, string baseFolder)
+        {
+            _cloudBlobClient = storageAccount.CreateCloudBlobClient();
+            _baseFolder = baseFolder;
+        }
+
+        public async Task RunAsync()
+        {
+            var containers = Directory.GetDirectories(_baseFolder);
+            foreach (var containerDir in containers)
+            {
+                var containerName = new DirectoryInfo(containerDir).Name;
+                Console.WriteLine($"Check and create container: {containerName}");
+
+                var cloudBlobContainer = _cloudBlobClient.GetContainerReference(containerName);
+                await cloudBlobContainer.CreateIfNotExistsAsync();
+
+                string[] files = Directory.GetFiles(containerDir, "*.*", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    Console.WriteLine($"[Begin] Container: {containerName} Upload File:{file}");
+                    var relativePath = file.Substring(containerDir.Length + 1);
+                    var blobName = relativePath.Replace('\\', '/');
+
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(blobName);
+                    try
+                    {
+                        var exist = await cloudBlockBlob.ExistsAsync();
+                        if (!exist)
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                try
+                                {
+                                    await cloudBlockBlob.UploadFromFileAsync(file);
+                                    Console.WriteLine($"[Finish] Container: {containerName} Upload File:{file}");
+                                    break;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.Error.WriteLine(ex);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/AzUtility.BlobUploader/Program.cs
+++ src/AzUtility.BlobUploader/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace AzUtility.BlobUploader
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option("-d", "Relative or absolute path to the folder that you want to upload.", CommandOptionType.SingleValue)]
+        [Required]
+        public string Dir { get; }
+
+        [Option("-c", "Storage connection string. Can be ignored when '-l' is specified.", CommandOptionType.SingleValue)]
+        public string ConnectionString { get; set; }
+
+        [Option("-l", "Use local storage emulator.", CommandOptionType.NoValue)]
+        public bool Local { get; }
+
+        public void OnExecute()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Use local storage emulator: {Local}");
+            if (Local && string.IsNullOrEmpty(ConnectionString))
+            {
+                ConnectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+            }
+
+            if (CloudStorageAccount.TryParse(ConnectionString, out var storageAccount))
+            {
+                var fullDir = Path.GetFullPath(Dir);
+                Console.WriteLine($"The folder to upload is: {fullDir}");
+                var uploader = new FolderUploader(storageAccount, fullDir);
+                uploader.RunAsync().Wait();
+            }
+            else
+            {
+                Console.Error.WriteLine("Connection String is invalid.");
+            }
         }
     }
 }
